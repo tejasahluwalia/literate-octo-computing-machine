40e293522f6102eafc6bbb5cc8ab6e8967f9b5f2
Tejas Ahluwalia <tejas@tujux.com>
2025-07-14 14:05:03 +0530

    added Utils

---
+++ SalesTax/Program.cs
@@ -0,0 +1,21 @@
+﻿using SalesTax;
+
+Console.WriteLine("Running SalesTax");
+
+double BASIC_SALES_TAX_RATE = 10.0;
+double IMPORT_TAX_RATE = 5.0;
+
+TaxRateCalculator taxRateCalculator = new(BASIC_SALES_TAX_RATE, IMPORT_TAX_RATE);
+Basket basket = new();
+
+var filename = args[0];
+
+var input = Utils.ReadFrom(filename);
+
+foreach (var line in input)
+{
+    // TODO: Parse input -> Products -> AddToBasket
+    Console.WriteLine(line);
+}
+
+ReceiptGenerator.PrintTotal(basket, taxRateCalculator);
