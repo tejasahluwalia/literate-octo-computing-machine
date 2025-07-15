30fae1fa4667bb36f4232c5626d2f8b99a7b62f2
Tejas Ahluwalia <tejas@tujux.com>
2025-07-15 18:39:45 +0530

    added cli

--- SalesTax/Program.cs
+++ SalesTax/Program.cs
@@ -1,14 +1,53 @@
 ﻿using SalesTax;
 
-Console.WriteLine("Running SalesTax");
+Console.WriteLine("Running Sales Tax");
+Console.WriteLine("Enter products one by one (format: '1 book at 12.49')");
+Console.WriteLine("Enter 'done' to checkout");
 
 double BASIC_SALES_TAX_RATE = 10.0/100;
 double IMPORT_TAX_RATE = 5.0/100;
 
 TaxRateCalculator taxRateCalculator = new(BASIC_SALES_TAX_RATE, IMPORT_TAX_RATE);
+var basket = new Basket();
 
-var filename = args[0];
-var input = Utils.ReadFrom(filename);
-var basket = OrderParser.ParseOrder(input);
+while (true)
+{
+    Console.Write("Enter product: ");
+    var input = Console.ReadLine();
+    
+    if (string.IsNullOrWhiteSpace(input))
+        continue;
 
+    if (string.Equals(input, "done"))
+        break;
+
+    try
+    {
+        var product = OrderParser.ParseProductLine(input);
+        var quantity = OrderParser.ParseQuantityFromLine(input);
+        basket.AddProduct(product, quantity);
+        Console.WriteLine($"Added {quantity} x {product.Name} to basket");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}");
+        Console.WriteLine("Please try again with format: '1 book at 12.49'");
+        continue;
+    }
+    
+    Console.WriteLine("\nCurrent basket:");
+    foreach (var item in basket.Products)
+    {
+        Console.WriteLine($"  {item.Value} x {item.Key.Name} - ${item.Key.Price:0.00} each");
+    }
+    
+    Console.Write("\nAdd more products? (y/n): ");
+    var response = Console.ReadLine();
+    if (response?.ToLower() == "n")
+        break;
+        
+    Console.WriteLine();
+}
+
+Console.WriteLine("\n--- RECEIPT ---");
 ReceiptGenerator.PrintTotal(basket, taxRateCalculator);
